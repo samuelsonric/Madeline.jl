284930612095ed7eedbd2d9b85dfa358e5b34622
Richard Samuelson <samuelson.rick@gmail.com>
2026-05-10 22:28:10 -0400

    clean

--- src/cholesky/sparse_cholesky.jl
+++ src/cholesky/sparse_cholesky.jl
@@ -1,8 +1,27 @@
+function SparseCholesky{T, I}(pattern::SparseMatrixCSC, k::Integer) where {T, I}
+    F = FChordalCholesky{:L, T, I}(pattern)
+    n = size(F, 1)
+
+    W = FMatrix{T}(undef, k, k)
+    prm = FVector{I}(undef, k)
+    ivp = FVector{I}(undef, k)
+    Mptr = FVector{I}(undef, F.L.S.nMptr)
+    Mval = FVector{T}(undef, F.L.S.nNval * 2)
+    Fval = FVector{T}(undef, F.L.S.nFval * 2)
+    temp = FVector{T}(undef, n)
+
+    return SparseCholesky(F, W, prm, ivp, Mptr, Mval, Fval, temp)
+end
+
 struct SparseCholesky{T, I} <: AbstractCholesky{T}
     F::FChordalCholesky{:L, T, I}
+    W::FMatrix{T}         # workspace for permuted W matrix
     prm::FVector{I}       # workspace for sorting permutation
     ivp::FVector{I}       # workspace for inverse permutation
-    W::FMatrix{T}         # workspace for permuted W matrix
+    Mptr::FVector{I}      # workspace for ldiv
+    Mval::FVector{T}      # workspace for ldiv
+    Fval::FVector{T}      # workspace for ldiv
+    temp::FVector{T}      # workspace for permuted rhs
 end
 
 function setzero!(chol::SparseCholesky{T}) where {T}
@@ -40,59 +59,108 @@ function addclique!(chol::SparseCholesky{T, I}, A::AbstractMatrix{T}, clique::Ab
 end
 
 function add_clique_impl!(L::FChordalTriangular{:N, :L, T, I}, W::AbstractMatrix{T}, ind::AbstractVector{I}) where {T, I}
-    S = L.S
-    n = length(ind)
+    n = convert(I, length(ind))
 
-    lo = ind[1]
-    hi = ind[n]
+    f = L.S.idx[ind[1]]
 
-    flo = S.idx[lo]
-    fhi = S.idx[hi]
+    D, res = diagblock(L, f)
+    B, sep = offdblock(L, f)
 
-    f = flo
-    k = one(I)
-
-    while f <= fhi
-        D, res = diagblock(L, f)
-        B, sep = offdblock(L, f)
-
-        rlo = first(res)
-        rhi = last(res)
-
-        if !isempty(sep)
-            slo = first(sep)
-            shi = last(sep)
-        end
-
-        while k <= n && ind[k] <= rhi
-            v = ind[k]
-
-            if v >= rlo
-                vloc = v - rlo + one(I)
-
-                sloc = one(I)
-
-                for j in k:n
-                    w = ind[j]
-
-                    if w <= rhi
-                        wloc = w - rlo + one(I)
-                        D[wloc, vloc] += W[j, k]
-                    elseif !isempty(sep) && w >= slo && w <= shi
-                        while sep[sloc] < w
-                            sloc += one(I)
-                        end
-
-                        B[sloc, vloc] += W[j, k]
-                    end
-                end
-            end
-
-            k += one(I)
-        end
-
-        f = S.pnt[f]
+    rlo = first(res)
+    rhi = last(res)
+    
+    if !isempty(sep)
+        slo = first(sep)
+        shi = last(sep)
     end
 
-    return L
+    for k in oneto(n)
+        v = ind[k]
+
+        if rhi < v
+            f = L.S.idx[v]
+
+            D, res = diagblock(L, f)
+            B, sep = offdblock(L, f)
+            
+            rlo = first(res)
+            rhi = last(res)
+            
+            if !isempty(sep)
+                slo = first(sep)
+                shi = last(sep)
+            end
+        end
+
+        vloc = v - rlo + one(I)
+        sloc = one(I)
+
+        for j in k:n
+            w = ind[j]
+
+            if w <= rhi
+                wloc = w - rlo + one(I)
+                D[wloc, vloc] += W[j, k]
+            elseif !isempty(sep) && w >= slo && w <= shi
+                while sep[sloc] < w
+                    sloc += one(I)
+                end
+
+                B[sloc, vloc] += W[j, k]
+            else
+                break
+            end
+        end
+    end
+
+    return
+end
+
+function ldiv_fwd!(chol::SparseCholesky{T}, b::AbstractVector{T}) where {T}
+    F = chol.F
+    n = length(b)
+
+    @inbounds for i in 1:n
+        chol.temp[i] = b[F.perm[i]]
+    end
+
+    @inbounds for i in 1:n
+        b[i] = chol.temp[i]
+    end
+
+    div_impl!(b, chol.Mptr, chol.Mval, chol.Fval, F.L, Val(:N), Val(:N), F.L.uplo)
+
+    return b
+end
+
+function ldiv_fwd!(chol::SparseCholesky{T}, B::AbstractMatrix{T}) where {T}
+    F = chol.F
+
+    @inbounds for j in axes(B, 2)
+        for i in axes(B, 1)
+            chol.temp[i] = B[F.perm[i], j]
+        end
+
+        for i in axes(B, 1)
+            B[i, j] = chol.temp[i]
+        end
+    end
+
+    div_impl!(B, chol.Mptr, chol.Mval, chol.Fval, F.L, Val(:N), Val(:N), F.L.uplo)
+
+    return B
+end
+
+function ldiv_bwd!(chol::SparseCholesky{T}, b::AbstractVector{T}) where {T}
+    F = chol.F
+
+    div_impl!(b, chol.Mptr, chol.Mval, chol.Fval, F.L, Val(:C), Val(:N), F.L.uplo)
+
+    @inbounds for i in eachindex(b)
+        chol.temp[F.perm[i]] = b[i]
+    end
+
+    copyto!(b, chol.temp)
+
+    return b
 end
